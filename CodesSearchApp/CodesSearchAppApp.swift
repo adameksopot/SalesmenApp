9e76394eda12670d8c29778d94ba972a8712030f
Adam Heinrich <adam.heinrich@lhsystems.com>
2025-03-09 08:09:32 +0100

    Initial Commit

---
+++ CodesSearchApp/CodesSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodesSearchAppApp.swift
+//  CodesSearchApp
+//
+//  Created by Adam Heinrich on 09/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CodesSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
